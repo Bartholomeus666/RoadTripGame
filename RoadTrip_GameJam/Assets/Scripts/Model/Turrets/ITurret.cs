3e5da92cd359125c878431fef98a5bc41c368331
Bartholomeus666 <kalhofernand@gmail.com>
2025-01-23 15:42:47 +0100

    Tower kinda works

--- RoadTrip_GameJam/Assets/Scripts/Model/Turrets/ITurret.cs
+++ RoadTrip_GameJam/Assets/Scripts/Model/Turrets/ITurret.cs
@@ -4,5 +4,10 @@ public interface ITurret
 {
     float _cooldownTime { get; set; }
     float _damage { get; set; }
-    float _range { get; set; }
+    float Range { get; set; }
+    bool _readyToFire { get; set; }
+    public GameObject Target { get; set; }
+
+    public void Shoot();
+    public void Update(float deltaTime);
 }
--- RoadTrip_GameJam/Assets/Scripts/Presenter/Turrets/SimpleTurretPresenter.cs
+++ RoadTrip_GameJam/Assets/Scripts/Presenter/Turrets/SimpleTurretPresenter.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 
 public class SimpleTurretPresenter : PresenterBase<SimpleTurretModel>
 {
+    [SerializeField] private float _range;
 
+    private void Start()
+    {
+        Model.Range = _range;
+    }
+    private void Update()
+    {
+        Model.Update(Time.deltaTime);
 
-
+    }
     protected override void HandlePropertyChanged(object sender, PropertyChangedEventArgs e)
     {
         
